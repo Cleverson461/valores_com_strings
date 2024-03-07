9a10e44bbc61b008802af8796f86ddd79d1c62cd
cleverson <cleverson461@gmail.com>
2024-03-06 22:19:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Este é com aspas duplas"); // String com Aspas duplas
+
+console.log('Este é com aspas simples'); // String aspas simples
+
+console.log(`E este é com template literals`); // String com template literals
